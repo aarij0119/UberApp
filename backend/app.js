a188991e8606af3a220af7dc2d87a0fd5926552f
Ash <154726037+aarij0119@users.noreply.github.com>
2025-02-11 12:59:17 +0530

    Modified server

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,22 @@ const app = express();
 
 dotnv.config();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    methods:'Post,Get'
+    methods: 'Post,Get',
+    credentials:true
 }))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("It's working on port 3000");
-})
+});
 
-app.listen(3000,()=>{
-    console.log("Server is runnig on 3000")
+let Port = 3000
+app.listen(Port, (err) => {
+    if (err) {
+        console.log("One error occured ", err.message)
+    } else {
+        console.log("Server is runnig on ", Port)
+    }
+
 })
